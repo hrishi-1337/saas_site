0836ace4a6983da644d1da0a088f33c523a97207
Hrishikesh Borkar <hrishikeshborkar11@gmail.com>
2016-12-08 06:46:43 +0000

    Added Contact Mailer

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,8 +1,24 @@
 class ContactsController < ApplicationController
+    def new
+        @contact = Contact.new
+    end
+    
     def create
+        @contact = Contact.new(contact_params)
+        if @contact.save
+            name = params[:contact][:name]
+            email = params[:contact][:email]
+            body = params[:contact][:body]
+            ContactMailer.contact_email(name,email,body).deliver
+            flash[:success] = 'Message sent succesfull'
+            redirect_to new_contacts_path
+        else
+            flash[:danger] = 'Error occured'
+            redirect_to new_contacts_path
     end
-
-    def fresh
-        @contact = Contact.fresh
-    end
+    
+    private
+        def contact_params
+            params.require(:contact).permit(:name, :email, :comments)
+        end
 end
