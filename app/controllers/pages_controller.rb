c1f458470b39c4539a99f826d0ce1f6ffb79fc15
Hrishikesh Borkar <hrishikeshborkar11@gmail.com>
2016-12-30 08:50:25 +0000

    Added Pro Registration

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,10 @@
 class PagesController < ApplicationController
     def home
+        @basic_plan = Plan.find(1)
+        @proplan = Plan.find(2)
     end
     
     def about
+        
     end
 end
